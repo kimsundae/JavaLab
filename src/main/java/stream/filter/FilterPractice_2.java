82120ad84ac2306b17adc86a45fd5ad6cfb51ed8
kimgeunbae <naa159@naver.com>
2023-11-02 23:54:18 +0900

    23:54 stream 연습

---
+++ src/main/java/stream/filter/FilterPractice_2.java
@@ -0,0 +1,33 @@
+package stream.filter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+// 스트림 만들기 818
+public class FilterPractice_2 {
+    public static void main(String[] args) {
+        // 컬렉션
+        List<Integer> list = Arrays.asList(1,2,3,4,5); // 가변인자
+        Stream<Integer> intStream = list.stream(); // list를 소스로 하는 컬렉션 생성
+        //배열
+        /*
+        Stream<T> Stream.of(T... values) // 가변인자
+        Stream<T> Stream.of(T[])
+        Stream<T> Arrays.stream(T[])
+        Stream<T> Arrays.stream(T[] array, int startInclusive, int endExclusive)
+        */
+
+        Stream<String> strStream1 = Stream.of("a","b","c"); // 가변인자
+        Stream<String> strStream2 = Stream.of(new String[]{"a","b","c"});
+        Stream<String> strStream3 = Arrays.stream(new String[]{"a","b","c"});
+        Stream<String> strStream4 = Arrays.stream(new String[]{"a","b","c"}, 0 , 3);
+        /*
+        IntStream IntStream.of(int... values) // Stream이 아니라 IntStream
+        IntStream IntStream.of(int[])
+        IntStream Arrays.stream(int[])
+        IntStream Arrays.stream(int[] array ,int startInclusive, int endExclusive)
+        */
+
+    }
+}
---
+++ src/main/java/stream/filter/TestDto.java
@@ -0,0 +1,17 @@
+package stream.filter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TestDto {
+    private int rrno;
+    private LocalDateTime rrtime;
+    private LocalDate rrstartdate;
+    private LocalDate rrenddate;
+    private LocalDateTime rrcheckin;
+    private LocalDateTime rrcheckout;
+    // private int mno;
+    private String mname;
+    private String mphone;
+    private int rno;
+}
